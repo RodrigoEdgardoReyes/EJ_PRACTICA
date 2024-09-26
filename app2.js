929e22dd46ef74cad80266a3aaa27d21af1b9006
Rodrigo Reyes <rodrigoreyes2902@gmail.com>
2024-09-25 21:35:06 -0600

    app 2 :tada::sparkles:

---
+++ app2.js
@@ -0,0 +1,79 @@
+let person = [
+  {
+    id: 1,
+    nombre: 'Albert Einstein',
+    apellido: 'Roust',
+    edad: 20,
+    deportes: ['futbol', 'basquetball', 'tennis'],
+    comidaFav: 'sushi',
+    altura: 1.70,
+    peso: 63 + ' KG'
+  },
+  {
+    id: 2,
+    nombre: 'Anonimo',
+    apellido: '?',
+    edad: 30,
+    deportes: ['voleibol', 'sotball', 'futbol'],
+    comidaFav: 'spagetti',
+    altura: 1.60,
+    peso: 54 + ' KG'
+  },
+  {
+    id: 3,
+    nombre: 'Alan Kay',
+    apellido: 'Brest',
+    edad: 25,
+    deportes: ['futbol', 'tennis'],
+    comidaFav: 'sushi',
+    altura: 1.70,
+    peso: 65 + ' KG'
+  },
+  {
+    id: 4,
+    nombre: 'Amelia Earhart',
+    apellido: 'Brest',
+    edad: 30,
+    deportes: ['futbol'],
+    comidaFav: 'ramen',
+    altura: 1.75,
+    peso: 65 + ' KG'
+  },
+  {
+
+    id: 5,
+    nombre: 'Aristotle',
+    apellido: 'Roust',
+    edad: 28,
+    deportes: ['futbol', 'voleibol'],
+    comidaFav: 'pizza',
+    altura: 1.65,
+    peso: 54 + ' KG'
+  },
+  {
+    id: 6,
+    nombre: 'Benjamin Franklin',
+    apellido: 'Eguizabal',
+    edad: 30,
+    deportes: ['futbol', 'voleibol', 'tennis', 'basquetball'],
+    comidaFav: 'sushi',
+    altura: 1.80,
+    peso: 70 + ' KG'
+  },
+  // {
+  //   'autor': 'Charles R. Swindoll',
+  //   'texto': 'La vida es 10% lo que te ocurre y 90% cómo reaccionas.'
+  // },
+  // {
+  //   'autor': 'Jane Goodall',
+  //   'texto': 'Lo que haces hace una diferencia. Y debes decidir si qué tipo de diferencia quieres hacer.'
+  // },
+  // {
+  //   'autor': 'John Muir',
+  //   'texto': 'El poder de la imaginación nos hace infinitos.'
+  // },
+  // {
+  //   'autor': 'Mark Twain',
+  //   'texto': 'Los dos días más importantes de tu vida son el día que naciste y el día que descubres por qué.'
+  // }
+];
