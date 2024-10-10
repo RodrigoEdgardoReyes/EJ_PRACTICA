d25e2baf4a85c3605deac2550ec08dcc45be3383
Rodrigo Reyes <rodrigoreyes2902@gmail.com>
2024-10-09 22:55:00 -0600

    permutaciones de una cadena :tada::sparkles:

---
+++ JavaScript/permutaciones-de-una-cadena.js
@@ -0,0 +1,12 @@
+/**
+ * Dada una cadena de caracteres únicos, escribe una función 
+ * que genere todas sus permutaciones posibles.
+ * 
+ * Problema:
+ * Escribe una función que reciba una cadena y devuelva una lista con todas sus permutaciones.
+ * 
+ * Ejemplo:
+ * Input: "ABC"
+ * 
+ * Output: ["ABC", "ACB", "BAC", "BCA", "CAB", "CBA"]
+ **/
