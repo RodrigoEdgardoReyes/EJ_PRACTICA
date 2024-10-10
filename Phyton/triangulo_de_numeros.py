49cf84173bff477391fe44a9e39897fd7a8b2a16
Rodrigo Reyes <rodrigoreyes2902@gmail.com>
2024-10-09 22:59:00 -0600

    triangulo de numeros

---
+++ Phyton/triangulo_de_numeros.py
@@ -0,0 +1,21 @@
+"""
+Dado un triángulo representado como una lista de listas de enteros, comienza desde la parte superior 
+y muévete hacia la parte inferior, sumando los números a medida que te mueves. Solo puedes moverte a 
+números que estén directamente debajo del número actual.
+  
+Problema: 
+Escribe una función que calcule la ruta con la suma máxima desde la parte superior hasta la inferior.
+ 
+Ejemplo:
+Input:
+[
+    [2],
+   [3, 4],
+  [6, 5, 7],
+ [4, 1, 8, 3]
+]
+ 
+Output: 18
+(La ruta es 2 -> 3 -> 5 -> 8)
+
+"""
