63e5f5590bf7290f51a367a2308b1f8aa116621a
Rodrigo Reyes <rodrigoreyes2902@gmail.com>
2024-10-11 22:11:54 -0600

    archivo eliminado :tada::sparkles:

--- Phyton/sudoku-solver.py
+++
@@ -1,8 +0,0 @@
-"""
-Dado un tablero de Sudoku parcialmente completado (una matriz 9x9), 
-escribe un programa que resuelva el Sudoku.
- 
-Problema: 
-Escribe una función que reciba una matriz 9x9 de enteros y resuelva el Sudoku, llenando
-las casillas vacías. Las casillas vacías estarán representadas por ceros.
-"""
