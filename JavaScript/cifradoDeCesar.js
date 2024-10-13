759a8cfad1c376ffccbfbd0f3d9e1d8e97b0e8db
Rodrigo Reyes <rodrigoreyes2902@gmail.com>
2024-10-12 23:18:32 -0600

    cifrado de cesar :tada::sparkles:

---
+++ JavaScript/cifradoDeCesar.js
@@ -0,0 +1,14 @@
+/**
+ * El cifrado de César es una de las técnicas de cifrado más antiguas. Consiste en desplazar
+ * cada letra de un mensaje por un número fijo de posiciones en el alfabeto.
+ * 
+ * Problema: 
+ * Escribe una función que cifre y descifre un mensaje usando 
+ * el cifrado de César con un desplazamiento dado.
+ * 
+ * Ejemplo:
+ * 
+ * Input: mensaje = "HELLO", desplazamiento = 3
+ * 
+ * Output (cifrado): "KHOOR"
+ */
