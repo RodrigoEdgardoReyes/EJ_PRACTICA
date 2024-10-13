52a9e9da3acee3b55c2ab936f43ed62a1dfcf911
Rodrigo Reyes <rodrigoreyes2902@gmail.com>
2024-10-12 23:20:08 -0600

    torre de hanoi :tada::sparkles:

---
+++ JavaScript/torreDeHanoi.js
@@ -0,0 +1,13 @@
+/**
+ * La Torre de Hanoi es un problema matemático que involucra tres postes y un número n de discos 
+ * de diferentes tamaños. Inicialmente, todos los discos están en el primer poste, y el objetivo
+ * es moverlos al tercer poste, respetando las siguientes reglas:
+ * 
+ * Solo puedes mover un disco a la vez.
+ * Un disco más grande no puede estar encima de un disco más pequeño.
+ * Solo puedes mover el disco que está en la parte superior de cada poste.
+ * 
+ * Problema: 
+ * Escribe una función que resuelva el problema de la Torre de Hanoi para n discos
+ * y registre los movimientos necesarios.
+ */
