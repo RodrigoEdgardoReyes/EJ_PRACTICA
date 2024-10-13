168827286d7ebdd9e5c80490e6f8707cc436e712
Rodrigo Reyes <rodrigoreyes2902@gmail.com>
2024-10-12 23:19:40 -0600

    sudoku solver :tada::sparkles:

---
+++ JavaScript/sudokuSolver.js
@@ -0,0 +1,10 @@
+/**
+ * Dado un tablero de Sudoku parcialmente completado (una matriz 9x9), 
+ * escribe un programa que resuelva el Sudoku.
+ * 
+ * Problema: 
+ * Escribe una función que reciba una matriz 9x9 de enteros y resuelva el Sudoku, llenando
+ * las casillas vacías. Las casillas vacías estarán representadas por ceros.
+
+ * 
+ *  */
