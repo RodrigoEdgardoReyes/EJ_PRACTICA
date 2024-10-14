8ede607c5d31f26748ac0d47b61c3700b66e9b01
Rodrigo Reyes <rodrigoreyes2902@gmail.com>
2024-10-13 23:50:28 -0600

    numeros narcisistas :tada::sparkles:

--- Phyton/numeros_narcisistas.py
+++ Phyton/numeros_narcisistas.py
@@ -14,9 +14,39 @@
 # (153 = 1^3 + 5^3 + 3^3)
  
 #  def numNarcisista():
+total = 0
+
 def numNarcisista (num):
-    num = input('Ingrese un numero entero')
+    # Pido al usuario que ingrese un numero
+    num = input('Ingrese un numero entero: ')
 
-    print(f"Numero entero: ", num)
+    # muestro el numero ingresado por el usuario
+    print('numero ingresado', num)
 
-numNarcisista()
+    # Convertir el numero a cadena str(num)
+    # Lo usare para comparar el resultado final
+    num_cadena = str(num)
+
+    # Contar el numero de digitos del numero entero ingresado por el user
+    # Lo usare para multiplicar cada digito por el total de digitos del 
+    # numero entero
+    num_digitos = len(num_cadena)
+
+    # muestro el numero de digitos
+    print('numero de digitos: ', num_digitos)
+
+    # Creo un diccionario para obtener los digitos por separado
+    digitos = []
+    resultado = 0
+
+    # for clave, valor in digitos.items():
+    # print(f'Clave: {clave}, Valor: {valor}')
+
+    for i in range(num_digitos):
+        digito = int(num[i]) ** num_digitos
+        print(f'digito: {digito}')
+        digitos.append(digito)
+
+
+# numNarcisista()
+print('El numero ingresado es: ', numNarcisista(total))
