352a78ca443bb07b4a1867d9b15dd000c9972b34
Rodrigo Reyes <rodrigoreyes2902@gmail.com>
2024-10-16 22:53:00 -0600

    subsecuencia comun mas larga :tada::sparkles:

---
+++ JavaScript/subsecuenciaComunMasLarga.js
@@ -0,0 +1,11 @@
+/**
+ * Dadas dos cadenas, encuentra la longitud de la subsecuencia común más larga entre ellas.
+ * La subsecuencia no necesita ser contigua, pero debe mantener el orden relativo.
+ * 
+ * Problema: 
+ * Escribe una función que calcule la longitud de la LCS entre dos cadenas. 
+ * 
+ * Ejemplo: 
+ * Input: "AGGTAB", "GXTXAYB" 
+ * Output: 4(La subsecuencia común más larga es "GTAB")
+ */
